3417c4153532181704a9fe5d3f0247461c8819a1
Jodecir Neto <jodecirneto@gmail.com>
2020-12-04 12:56:07 -0300

    gameover

--- js/script.js
+++ js/script.js
@@ -46,8 +46,27 @@ function armor() {
   }
 
   if (armorAtual==0) {
-    $("#armor").css("background-image", "url(img/hud.png)");
+    $("#armor").css("background-image", "url(img/hud.png)"); 
+    gameOver();
   }
+}
+
+function gameOver() {
+	fimdejogo=true;
+	musica.pause();
+	somGameover.play();
+	
+	window.clearInterval(jogo.timer);
+	jogo.timer=null;
+	
+	$("#jogador").remove();
+	$("#inimigo1").remove();
+	$("#inimigo2").remove();
+	$("#amigo").remove();
+	
+	$("#fundoGame").append("<div id='fim'></div>");
+	
+	$("#fim").html("<h1> Game Over </h1>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
 }
 
 function movefundo() {
